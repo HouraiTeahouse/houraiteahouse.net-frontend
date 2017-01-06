8ead270668beab2ee529e8502d965728500d1771
James Liu <jamessliu@google.com>
2017-01-05 18:46:44 -0800

    Add angular-translate packages

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ import angularUiRouter from 'angular-ui-router';
 import angularUiBootstrap from 'angular-ui-bootstrap';
 import 'angulartics';
 import angularticsGA from 'angulartics-google-analytics';
+import 'angular-translate'
+import 'angular-translate-loader-static-files'
 
 import './appControllersModule.js';
 import './appServicesModule.js';
@@ -25,6 +27,7 @@ var app = angular.module('houraiteahouse', [
     angularCookies,
     'angulartics',
     angularticsGA,
+    'pascalprecht.translate',
     'appControllers',
     'appDirectives',
     'appServices'
@@ -37,15 +40,25 @@ var options = {
 };
 
 // Main app configuration
-app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider',
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', '$translateProvider',
   function($stateProvider,
       $urlRouterProvider,
       $locationProvider,
-      $httpProvider) {
+      $httpProvider,
+      $translateProvider) {
 
     // Remove unnecessary and ugly '#' characters in URL
     // for browsers that support HTML5 mode.
     $locationProvider.html5Mode(true);
+
+    // Load language information only when needed
+    // Cache the language information so that it isn't repeatedly fetched
+    $translateProvider.useLocalStorage();
+
+    $translateProvider.determinePreferredLanguage();
+
+    $translateProvider.fallbackLanguage('en');
+
 
     $urlRouterProvider.otherwise('/404');
 
